0f8a2fbcd88bed01bf4b8b92daea7dd2e0955f6a
Matvei <matvei@Matveis-MacBook-Pro.local>
2023-09-16 13:32:45 +0200

    changed Product class

--- CaloriesTracker/CaloriesTracker/models/Product.cs
+++ CaloriesTracker/CaloriesTracker/models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 namespace CaloriesTracker
 {
     public class Product
@@ -7,7 +8,18 @@ namespace CaloriesTracker
         public string Img { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
+
+        // Additional properties for nutritional information
+        public double Calories { get; set; }
+        public double Proteins { get; set; }
+        public double Carbs { get; set; }
+        public double Fats { get; set; }
+
         // Add other properties as needed
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
-
